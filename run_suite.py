13b69984d4eb699d103f3cb52abb26d3b30bac78
Jenny <1305633200@qq>
2020-02-16 21:53:16 +0800

    注释

--- run_suite.py
+++ run_suite.py
@@ -1,7 +1,7 @@
-import unittest
-
-from tools.HTMLTestRunner import HTMLTestRunner
-
-suite = unittest.defaultTestLoader.discover("./scripts", pattern="test*.py")
-with open("./report/report.html", "wb", encoding="utf-8") as f:
-    HTMLTestRunner(stream=f).run(suite)
+# import unittest
+#
+# from tools.HTMLTestRunner import HTMLTestRunner
+#
+# suite = unittest.defaultTestLoader.discover("./scripts", pattern="test*.py")
+# with open("./report/report.html", "wb") as f:
+#     HTMLTestRunner(stream=f).run(suite)
